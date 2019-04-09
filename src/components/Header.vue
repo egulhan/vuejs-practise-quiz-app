63609c96335ce034b86d9b6db17f7b52b7fa6ef7
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-04-09 18:00:49 +0300

    vuejs practise - quiz app

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template><div>
+    <b-nav tabs>
+        <b-nav-item disabled>Quiz APP</b-nav-item>
+        <b-nav-item disabled>Counter: 12/60</b-nav-item>
+    </b-nav>
+</div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template slot="lead">
+                One Question Here?
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+                List of answers here
+            </p>
+
+            <b-button variant="primary" href="#">Send</b-button>
+            <b-button variant="success" href="#">Next ></b-button>
+        </b-jumbotron>
+    </div>
+</template>
